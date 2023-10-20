dbba18d1f95a2b0c9e5dc3d6e3933d0efdb9a623
qzz031219 <146476099+qzz031219@users.noreply.github.com>
2023-10-20 11:16:19 -0700

    Update main.py

--- assignment 2/main.py
+++ assignment 2/main.py
@@ -4,7 +4,7 @@ from js import document
 data = None
 
 def setup():
-  p5.createCanvas(300, 300)
+  p5.createCanvas(400, 400)
   print('hello p5!')
 
 def draw():
@@ -16,9 +16,18 @@ def draw():
   circle_size = int(data)
   p5.noStroke()
   p5.fill(150)
-  p5.ellipse(150, 150, circle_size, circle_size)
+  #p5.ellipse(150, 150, circle_size, circle_size)
+  p5.push()
+  angle = int(data)
+  p5.translate(p5.width/2,p5.height/2)
+  #degree to radians:
+  p5.rotate(p5.radians(angle))
+  p5.rectMode(p5.CENTER)
+  p5.rect(0,0,100,100)
+  p5.pop()
+  
+
+
 
 def print_test(x):
   print(x)
-
-  
