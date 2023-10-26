9601d70e22aa11b4b5054f676ee1275c104ff726
qzz031219 <146476099+qzz031219@users.noreply.github.com>
2023-10-26 14:39:55 -0700

    Update main.py

--- assignment 2/main.py
+++ assignment 2/main.py
@@ -2,32 +2,46 @@ import js as p5
 from js import document
 
 data = None
+isSquare = True
+base_color = (134, 255, 203) 
+shape_positions = [] 
+shape_sizes = [] 
+g_value = 128 
+
+def adjust_green(rgb_color, g_value):
+    r, _, b = rgb_color
+    return (r, g_value, b)
 
 def setup():
-  p5.createCanvas(400, 400)
-  print('hello p5!')
+    p5.createCanvas(1800, 1000) 
+    p5.background(0) 
+
+    for i in range(500):
+        x = p5.random(p5.width)
+        y = p5.random(p5.height)
+        size = p5.random(50, 100)  
+        shape_positions.append((x, y))
+        shape_sizes.append(size)
 
 def draw():
-  p5.background(255)
+    global data, isSquare, g_value, shape_positions, shape_sizes
+    data = document.getElementById("data").innerText
 
-  global data
-  data = document.getElementById("data").innerText
-  
-  circle_size = int(data)
-  p5.noStroke()
-  p5.fill(150)
-  #p5.ellipse(150, 150, circle_size, circle_size)
-  p5.push()
-  angle = int(data)
-  p5.translate(p5.width/2,p5.height/2)
-  #degree to radians:
-  p5.rotate(p5.radians(angle))
-  p5.rectMode(p5.CENTER)
-  p5.rect(0,0,100,100)
-  p5.pop()
-  
+    if data == 'TOGGLE_SHAPE':
+        isSquare = not isSquare
+        shape_positions = [(p5.random(p5.width), p5.random(p5.height)) for _ in range(500)]
+        shape_sizes = [p5.random(50, 150) for _ in range(500)]
+    else:
+        g_value = int(data) 
 
+        
+    p5.background(0)
 
+    for (x, y), size in zip(shape_positions, shape_sizes): 
+        r, g, b = adjust_green(base_color, g_value)
+        p5.fill(r, g, b)
 
-def print_test(x):
-  print(x)
+        if isSquare:
+            p5.rect(x, y, size, size)
+        else:
+            p5.ellipse(x, y, size, size)
