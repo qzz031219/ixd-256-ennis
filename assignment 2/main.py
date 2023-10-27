9b8563bf8496b106ca53fd4a04733165e05a6da3
qzz031219 <146476099+qzz031219@users.noreply.github.com>
2023-10-27 08:48:26 -0700

    Commit to project 2

--- assignment 2/main.py
+++ assignment 2/main.py
@@ -29,8 +29,8 @@ def draw():
 
     if data == 'TOGGLE_SHAPE':
         isSquare = not isSquare
-        shape_positions = [(p5.random(p5.width), p5.random(p5.height)) for _ in range(500)]
-        shape_sizes = [p5.random(50, 150) for _ in range(500)]
+        shape_positions = [(p5.random(p5.width), p5.random(p5.height)) for i in range(500)]
+        shape_sizes = [p5.random(50, 150) for i in range(500)]
     else:
         g_value = int(data) 
 
