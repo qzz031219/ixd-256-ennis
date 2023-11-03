42f81bc2537f42ce109d0db62aeaebe30cf46918
qzz031219 <146476099+qzz031219@users.noreply.github.com>
2023-11-02 21:58:52 -0700

    updated v

--- assignment 2/main.py
+++ assignment 2/main.py
@@ -9,14 +9,13 @@ shape_sizes = []
 g_value = 128 
 
 def adjust_green(rgb_color, g_value):
-    r, _, b = rgb_color
+    r = rgb_color[0] 
+    b = rgb_color[2] 
     return (r, g_value, b)
 
 def setup():
     p5.createCanvas(1800, 1000) 
-    p5.background(0) 
-
-    for i in range(500):
+    for i in range(300):
         x = p5.random(p5.width)
         y = p5.random(p5.height)
         size = p5.random(50, 100)  
@@ -24,20 +23,29 @@ def setup():
         shape_sizes.append(size)
 
 def draw():
+    p5.background(40)
     global data, isSquare, g_value, shape_positions, shape_sizes
     data = document.getElementById("data").innerText
 
     if data == 'TOGGLE_SHAPE':
+        if isSquare:
+            isSquare = True
+        else:
+            isSquare = False
         isSquare = not isSquare
-        shape_positions = [(p5.random(p5.width), p5.random(p5.height)) for i in range(500)]
-        shape_sizes = [p5.random(50, 150) for i in range(500)]
+        shape_positions = [] 
+        shape_sizes = []
+        for i in range(300):
+            shape_positions.append((p5.random(p5.width), p5.random(p5.height)))
+        for i in range(300):
+            shape_sizes.append(p5.random(50, 150))
+
     else:
         g_value = int(data) 
 
-        
-    p5.background(0)
-
-    for (x, y), size in zip(shape_positions, shape_sizes): 
+    for i in range(len(shape_positions)):
+        x, y = shape_positions[i]
+        size = shape_sizes[i]
         r, g, b = adjust_green(base_color, g_value)
         p5.fill(r, g, b)
 
